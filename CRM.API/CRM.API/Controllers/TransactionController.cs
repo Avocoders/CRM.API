def6e94e4d82b09ae1c5e1f1ff2c872cc8f68b11
Vadim <auxilant@gmail.com>
2020-07-09 18:34:00 +0300

    Create TransactionController
    TransactionController and transaction/transfer input models

---
+++ CRM.API/CRM.API/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TransactionController : Controller
+    {
+        private static readonly HttpClient httpClient;
+
+        static TransactionController()
+        {
+            httpClient = new HttpClient();
+        }
+
+
+    }
+}
