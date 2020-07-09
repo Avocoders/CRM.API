a775489498b3aacfc762fcf8ad457adee86c3020
Vadim <auxilant@gmail.com>
2020-07-09 19:19:39 +0300

    CreateTransferTransaction method

--- CRM.API/CRM.API/Controllers/TransactionController.cs
+++ CRM.API/CRM.API/Controllers/TransactionController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TransactionStore.API.Models.Input;
+using Newtonsoft.Json;
 
 namespace CRM.API.Controllers
 {
@@ -19,6 +22,14 @@ namespace CRM.API.Controllers
             httpClient = new HttpClient();
         }
 
-
+        [HttpPost("transfer")] // прописать BadRequests
+        public async Task<ActionResult<List<long>>> CreateTransferTransaction([FromBody] TransferInputModel transactionModel)
+        {
+            var jsonContent = new StringContent(JsonConvert.SerializeObject(transactionModel), Encoding.UTF8, "application/json");
+            var response = await httpClient.PostAsync("https://localhost:44388/transaction/transfer", jsonContent);
+            string content = await response.Content.ReadAsStringAsync();
+            List<long> list = JsonConvert.DeserializeObject<List<long>>(content);
+            return Ok(list);
+        }
     }
 }
