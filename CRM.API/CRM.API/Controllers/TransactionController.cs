fd5d83660be1cc9702a868e4b22cfb0b412cf899
Daria <baodaria@gmail.com>
2020-07-20 01:16:14 +0300

    added badrequests

--- CRM.API/CRM.API/Controllers/TransactionController.cs
+++ CRM.API/CRM.API/Controllers/TransactionController.cs
@@ -7,6 +7,7 @@ using TransactionStore.API.Models.Input;
 using Newtonsoft.Json;
 using CRM.Data;
 using CRM.API.Models.Output;
+using CRM.API.Models.Input;
 
 namespace CRM.API.Controllers
 {
@@ -25,8 +26,10 @@ namespace CRM.API.Controllers
         [HttpPost("transfer")]
         public async Task<ActionResult<List<long>>> CreateTransferTransaction([FromBody] TransferInputModel transactionModel)
         {
-            if (_repo.GetById(transactionModel.DestinationLeadId) is null) return BadRequest("The user is deleted");
-
+            if (_repo.GetById(transactionModel.LeadId).Data is null) return BadRequest("The user is not found");
+            if (_repo.GetById(transactionModel.DestinationLeadId).Data is null) return BadRequest("The user is not found");
+            if (transactionModel.Amount <= 0) return BadRequest("The amount is missing");
+            if (transactionModel.CurrencyId <= 0) return BadRequest("The currency is missing");
             var jsonContent = new StringContent(JsonConvert.SerializeObject(transactionModel), Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("https://localhost:44388/transaction/transfer", jsonContent);
             string content = await response.Content.ReadAsStringAsync();
@@ -36,6 +39,9 @@ namespace CRM.API.Controllers
         [HttpPost("withdraw")]
         public async Task<ActionResult<long>> CreateWithdrawTransaction([FromBody] TransactionInputModel transactionModel)
         {
+            if (_repo.GetById(transactionModel.LeadId) is null) return BadRequest("The user is not found");
+            if (transactionModel.Amount <= 0) return BadRequest("The amount is missing");
+            if (transactionModel.CurrencyId <= 0) return BadRequest("The currency is missing");
             var jsonContent = new StringContent(JsonConvert.SerializeObject(transactionModel), Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("https://localhost:44388/transaction/withdraw", jsonContent);
             string content = await response.Content.ReadAsStringAsync();
@@ -45,6 +51,9 @@ namespace CRM.API.Controllers
         [HttpPost("deposit")]
         public async Task<ActionResult<long>> CreateDepositTransaction([FromBody] TransactionInputModel transactionModel)
         {
+            if (_repo.GetById(transactionModel.LeadId) is null) return BadRequest("The user is not found");
+            if (transactionModel.Amount <= 0) return BadRequest("The amount is missing");
+            if (transactionModel.CurrencyId <= 0) return BadRequest("The currency is missing");
             var jsonContent = new StringContent(JsonConvert.SerializeObject(transactionModel), Encoding.UTF8, "application/json");
             var response = await _httpClient.PostAsync("https://localhost:44388/transaction/deposit", jsonContent);
             string content = await response.Content.ReadAsStringAsync();
@@ -54,6 +63,8 @@ namespace CRM.API.Controllers
         [HttpGet("by-lead-id/{leadId}")]
         public async Task<ActionResult<List<TransferOutputModel>>> GetTransactionsByLeadId(long leadId)
         {
+            
+            if (leadId <= 0) return BadRequest("Lead was not found");
             var response = await _httpClient.GetAsync($"https://localhost:44388/transaction/by-lead-id/{leadId}");
             string content = await response.Content.ReadAsStringAsync();
             return StatusCode((int)response.StatusCode, content);
@@ -62,6 +73,7 @@ namespace CRM.API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<TransactionOutputModel>> GetTransactionById(long id)
         {
+            if (id <= 0) return BadRequest("Lead was not found");
             var response = await _httpClient.GetAsync($"https://localhost:44388/transaction/{id}");
             string content = await response.Content.ReadAsStringAsync();
             return StatusCode((int)response.StatusCode, content);
@@ -70,18 +82,12 @@ namespace CRM.API.Controllers
         [HttpGet("{leadId}/balance/{currencyId}")]
         public async Task<ActionResult<decimal>> GetBalanceByLeadIdInCurrency(long leadId, byte currencyId)
         {
+            if (leadId <= 0) return BadRequest("Lead was not found");
+            if (currencyId <= 0) return BadRequest("Currency was not found");
             var response = await _httpClient.GetAsync($"https://localhost:44388/transaction/{leadId}/balance/{currencyId}");
             string content = await response.Content.ReadAsStringAsync();
             return StatusCode((int)response.StatusCode, content);
         }
-
-        /*
-        [HttpGet("by-lead-id/{leadId}/range-date")]
-        public ActionResult<List<TransferOutputModel>> GetRangeDateTransactionByLeadId([FromBody] RangeDateInputModel rangeModel)
-        {
-            return _mapper.ConvertTransferTransactionDtosToTransferOutputModels(_repo.GetRangeDateByLeadId(_mapper.ConvertRangeDateInputModelToRangeDateDto(rangeModel)));
-        }
-        */
 
     }
 }
