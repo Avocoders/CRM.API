b32370ba0a181addd018640d9ec2b98616445423
Slava <zaharko@yandex.ru>
2020-07-21 18:28:50 +0300

    changes to dto, inputmodels and repository

--- CRM.API/CRM.API/Models/Input/SearchParametersInputModel.cs
+++ CRM.API/CRM.API/Models/Input/SearchParametersInputModel.cs
@@ -2,17 +2,26 @@
 {
     public class SearchParametersInputModel
     {
-        public int? RoleId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Patronymic { get; set; }
-        public string Login { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public int? CityId { get; set; }
-        public string Address { get; set; }
-        public string BirthDate { get; set; }
-        public string RegistrationDate { get; set; }
-        public bool? IncludeDeleted { get; set; }
-    }
+		public int? RoleId { get; set; }
+		public int? FirstNameSearchMode { get; set; }
+		public string FirstName { get; set; }
+		public int? LastNameSearchMode { get; set; }
+		public string LastName { get; set; }
+		public int? PatronymicSearchMode { get; set; }
+		public string Patronymic { get; set; }
+		public int? LoginSearchMode { get; set; }
+		public string Login { get; set; }
+		public int? PhoneSearchMode { get; set; }
+		public string Phone { get; set; }
+		public int? EmailSearchMode { get; set; }
+		public string Email { get; set; }
+		public int? CityId { get; set; }
+		public int? AddressSearchMode { get; set; }
+		public string Address { get; set; }
+		public string BirthDateBegin { get; set; }
+		public string BirthDateEnd { get; set; }
+		public string RegistrationDateBegin { get; set; }
+		public string RegistrationDateEnd { get; set; }
+		public bool? IncludeDeleted { get; set; }
+	}
 }
