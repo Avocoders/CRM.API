f71d8207dc5b0a9456197488f36ce07f3db3a88b
Slava <zaharko@yandex.ru>
2020-08-04 20:04:35 +0300

    Add Migration for  CRM.DataBase

---
+++ CRM.API/DatabaseCRM/dbo/Migration/v1.1/Account_Add_Or_Update.sql
@@ -0,0 +1,27 @@
+﻿create procedure Account_Add_Or_Update
+	@id			bigint,
+	@leadId bigint,
+	@balance bigint,
+	@currencyId int
+	as
+	begin
+		merge  dbo.[Account] a
+		using (values (@id)) n(Id) 
+		on a.Id = n.Id 
+		when matched and IsDeleted=0
+			then update 
+				set a.Balance=@balance,
+					a.СurrencyId=@currencyId						
+		when not matched 
+			then insert (LeadId,
+						Balance,
+						СurrencyId,
+						IsDeleted) 
+				values (@leadId, 
+						@balance, 
+						@currencyId,
+						default);
+		declare @inserted bigint = CAST(SCOPE_IDENTITY() as [bigint])
+		if @id > 0 exec Account_GetById @id
+		else exec Account_GetById @inserted
+	end
---
+++ CRM.API/DatabaseCRM/dbo/Migration/v1.1/Account_GetById.sql
@@ -0,0 +1,8 @@
+﻿create procedure Account_GetById
+	@accoundId bigint
+	as
+	begin
+		select a.Id, l.FirstName, l.LastName, l.BirthDate, a.Balance, a.СurrencyId, a.IsDeleted from dbo.[Account] a
+		inner join [Lead] l on l.Id=a.LeadId
+		where a.Id=@accoundId 
+	end
