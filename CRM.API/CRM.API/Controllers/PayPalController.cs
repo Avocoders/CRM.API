65aee0c9f181a53140a769caf49959e637f93a30
Anastasia Kuden <anastasya.kuden@gmail.com>
2020-08-26 18:20:17 +0300

    PayPal connect with TransactionStore

--- CRM.API/CRM.API/Controllers/PayPalController.cs
+++ CRM.API/CRM.API/Controllers/PayPalController.cs
@@ -10,27 +10,28 @@ using CRM.API.Models.Input;
 using RestSharp.Authenticators;
 using CRM.API.Models;
 using Newtonsoft.Json;
+using TransactionStore.API.Models.Input;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Globalization;
 
 namespace CRM.API.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class PayPalController : Controller
+    public class PayPalController : TransactionController
     {
-        private RestClient _payPalClient;
-        private readonly ILeadRepository _repo;
+        private RestClient _payPalClient;       
         //private readonly ILogger _logger;
         private const string _paymentUrl = "payments/payment";
         private const string _createToken = "oauth2/token";
         private const string userName = "AUQVTtwW6FAGCRUZNVcFU9BffNtzw-ukYIQmW1pk-uODKcB_Y3Ei6NfE25lC8VPwqjFcCMS3pokeQCy_";
-        private const string password = "EEGtuAyQIHSYEgmV9VfA7I_7XqaKrY566l1NIJytW8z19Vbp-LiLxxYwNlrpF7Ga-4sLCY7BbX5T9Du1";
-        private readonly string _options;
+        private const string password = "EEGtuAyQIHSYEgmV9VfA7I_7XqaKrY566l1NIJytW8z19Vbp-LiLxxYwNlrpF7Ga-4sLCY7BbX5T9Du1";     
+        
 
-        public PayPalController(ILeadRepository repo, IOptions<UrlOptions> options/*, ILogger logger*/)
-        {
-            _repo = repo;
-            _payPalClient = new RestClient(options.Value.PayPalUrl);
-            _options = options.Value.PayPalUrl;
+        public PayPalController(ILeadRepository repo, IOptions<UrlOptions> options/*, ILogger logger*/) : base(repo, options)
+        {           
+            _payPalClient = new RestClient(options.Value.PayPalUrl);           
             /*_logger = logger;*/
         }
 
@@ -50,26 +51,37 @@ namespace CRM.API.Controllers
         [HttpPost(_paymentUrl)]
         public async Task<ActionResult<string>> CreatePayPalPayment([FromBody] PaypalInputModel paypalInputModel)
         {
-            var tmp = GetPayPalToken().Value;
-            //_payPalClient = new RestClient(_options);
+            var tmp = GetPayPalToken().Value;           
             _payPalClient.AddDefaultHeader("Authorization", $"Bearer {tmp}");
-            var restRequest = new RestRequest(_paymentUrl, Method.POST, DataFormat.Json);
-            //restRequest.AddHeader("Authorization", "Bearer " + tmp.Value);
+            var restRequest = new RestRequest(_paymentUrl, Method.POST, DataFormat.Json);            
             restRequest.AddJsonBody(paypalInputModel);
             var tmp2 = _payPalClient.Execute<string>(restRequest).Data;
             return tmp2;           
         }
 
-        [HttpPost(_paymentUrl + "/{paymentId}/execute")]
-        public async Task<ActionResult<string>> ExecutePayPalPayment (string paymentId, [FromBody] ExecuteInputModel model)
+        [HttpPost(_paymentUrl + "/{paymentId}/execute/{accountId}")]
+        public async void ExecutePayPalPayment (string paymentId, [FromBody] ExecuteInputModel model, long accountId)
         {
-            var tmp = GetPayPalToken().Value;
-            //_payPalClient = new RestClient(_options);
+            var tmp = GetPayPalToken().Value;            
             _payPalClient.AddDefaultHeader("Authorization", $"Bearer {tmp}");
             var restRequest = new RestRequest($"{_paymentUrl}/{paymentId}/execute", Method.POST, DataFormat.Json);
             restRequest.AddJsonBody(new { payer_id  = model.payerId });
-            var tmp2 = _payPalClient.Execute<string>(restRequest).Data;
-            return tmp2; //после проверки дописываем отправку в транзакшн стор, если не вернет успех, то рефанд делаем
+            var tmp2 = _payPalClient.Execute<PaypalInputModel>(restRequest);          
+            if((int)tmp2.StatusCode == 200)
+            {
+                var tmp4 = (tmp2.Data.transactions[0].amount.total);
+                var tmp3 = Decimal.Parse(tmp4,
+                  NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+
+                //Convert.ToDouble(tmp4);
+                TransactionInputModel transactionModel = new TransactionInputModel();
+                transactionModel.AccountId = accountId;
+                transactionModel.Amount = tmp3;
+                var result = CreateDepositTransaction(transactionModel);                
+            }
+          
+            //после проверки дописываем отправку в транзакшн стор, если не вернет успех, то рефанд делаем
+
         }
     }
 }
